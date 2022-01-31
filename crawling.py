1db55ad2d8dd4a98304808db283917f649d2dd85
Jo Byeong Wook <whquddnr97@gmail.com>
2022-01-31 18:50:06 +0900

    TODO : HTML parsing

--- crawling.py
+++ crawling.py
@@ -1,38 +1,55 @@
 from driver import Driver
 from bs4 import BeautifulSoup
 import math
-
+import time
 def get_policy_num(driver):
+    # 청년정책 통합검색 사이트의 URL과, 주거/금융 유형 세부검색을 위한 XPath
     youthCenter = 'https://www.youthcenter.go.kr'
     policy_housing_finance = ['//*[@id="pc_gnb"]/ul/li[1]/a/span',
                               '//*[@id="srchFrm"]/div[2]/div[1]/div[2]/div[2]/ul/li[3]/span/button',
                               '//*[@id="srchFrm"]/div[2]/div[1]/div[5]/div[2]/span/label',
                               '//*[@id="srchFrm"]/div[2]/div[2]/a[2]']
+
+    count = 0
     policy_numbers = []
 
+    # 청년정책 통합검색 사이트 접속
     driver.get_url(youthCenter)
     print("사이트 접속 성공")
 
+    # 세부검색을 위해 지정된 버튼을 순차적으로 클릭
     for order, xpath in enumerate(policy_housing_finance):
         driver.find_xpath_element(xpath).click()
     print("정책 선택 완료")
 
+    # 검색된 정책의 총 개수, 페이지 수를 저장
     total_num = int(driver.find_xpath_element('//*[@id="srchFrm"]/div[4]/div[1]/div[1]/strong/span').text)
     total_page_num = math.ceil(total_num / 12)
     print('정책 갯수 : ', total_num, '페이지 수 : ', total_page_num)
 
-    aTags = driver.find_css_selector_elements('#srchFrm > div.sch-result-wrap.compare-result-list > div.result-list-box > ul > li > a')
-    for aTag in aTags:
-        policy_number = str(aTag.get_attribute('id')[8:])
-        policy_numbers.append(policy_number)
+    # 페이지 수 만큼 순회
+    for page in range(total_page_num):
+        # 한 페이지에 보여지는 정책들의 정책번호를 받아서 저장
+        aTags = driver.find_css_selector_elements(
+            '#srchFrm > div.sch-result-wrap.compare-result-list > div.result-list-box > ul > li > a')
+        for aTag in aTags:
+            policy_number = str(aTag.get_attribute('id')[8:])
+            policy_numbers.append(policy_number)
+
+        # 다음 페이지로 이동
+        count += 1
+        driver.execute_script("fn_move({})".format(count + 1))
 
     print("{}개 정책의 정책번호 저장 완료".format(len(policy_numbers)))
 
     return policy_numbers
 
-def inject_script(driver):
+def get_policy_detail(driver, policy_numbers):
+    html_list = []
+    # 정책 번호를 이용하여 상세 페이지로 이동하는 자바스크립트
+    # JS의 스코핑 룰 때문에 전역함수로 선언해주어야 함
     script = """
-    function f_Detail(key){
+    window.f_Detail = function f_Detail(key){
         $("#srchAge").val($("#srchAge").val().replace(/[^0-9]/gi,""));
         $("#bizId").val(key);
         var srchFrm = document.srchFrm;
@@ -40,19 +57,33 @@ def inject_script(driver):
         srchFrm.submit();}
     """
 
-    driver.execute_script(script)
-    driver.execute_script("f_Detail('R2021021800114')")
+    # 저장된 정책번호를 순차적으로 돌면서, 상세 페이지를 보여주는 자바스크립트를 실행
+    # 상세 페이지로 이동 한 뒤, 해당 페이지를 html로 저장
+    for policy_number in policy_numbers:
+        driver.execute_script(script)
+        driver.execute_script("f_Detail('{}')".format(policy_number))
+        html_list.append(driver.get_html())
+    return html_list
 
-def parsing_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
+def parse_html(html_list):
+    for html in html_list:
+        soup = BeautifulSoup(html, 'html.parser')
 
 if __name__ == "__main__":
-    print("크롤링 시작")
+    # 초기화
     driver = Driver()
+    print("크롤링 시작")
 
+    # 정책 번호 받아오기
     policy_numbers = get_policy_num(driver)
+
+    # 정책 번호에 해당하는 html 받아오기
+    html_list = get_policy_detail(driver, policy_numbers)
+
+    # 받아온 html을 파싱
+    parse_html(html_list)
+
+    # 종료
     driver.exit()
-    #parsing_html(result)
 
 
--- driver.py
+++ driver.py
@@ -6,12 +6,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Driver Class
 class Driver:
+    # 초기화
+    # 차단 방지를 위한 User-Agent 설정
+    # 브라우저 창이 안보이도록 설정
+    # ChromeDriver를 사용자 버전에 맞추어 설치
+    # 웹페이지 로딩을 암묵적으로 대기함
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument(
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
         )
+        chrome_options.add_argument('headless')
         self.driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
                               options = chrome_options)
         self.driver.implicitly_wait(5)
@@ -19,38 +26,46 @@ class Driver:
     def __call__(self):
         return self.driver
 
+    # Url 접속
     def get_url(self, url):
         self.driver.get(url)
 
+    # Html 저장
     def get_html(self):
         return self.driver.page_source
 
+    # XPath로 특정 요소를 찾음
+    # 해당하는 요소의 로딩을 기다리고, 로딩 되지 않았다면 exception 발생시킴
     def find_xpath_element(self, xpath):
         return WebDriverWait(self.driver, 5, poll_frequency = 1).until(
             EC.presence_of_element_located((By.XPATH, xpath)),
             message = 'XPATH element not found'
         )
 
+    # css selector로 특정 요소를 찾음
+    # 해당하는 요소의 로딩을 기다리고, 로딩 되지 않았다면 exception 발생시킴
     def find_css_selector_element(self, css_selector):
         return WebDriverWait(self.driver, 5, poll_frequency = 1).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)),
             message = 'CSS_SELECTOR element not found'
         )
 
+    # css selector로 다수의 요소를 찾음
+    # 해당하는 요소의 로딩을 기다리고, 로딩 되지 않았다면 exception 발생시킴
     def find_css_selector_elements(self, css_selector):
         return WebDriverWait(self.driver, 5, poll_frequency = 1).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, css_selector)),
             message = 'CSS_SELECTOR elements not found'
         )
 
+    # 자바스크립트를 실행시킴
     def execute_script(self, script):
         self.driver.execute_script(script)
 
+    # 자바스크립트의 Onclick 이벤트를 실행시킴
     def execute_onclick(self, link):
         self.driver.execute_script("arguments[0].click();", link)
 
-    def wait(self, time):
-        self.driver.implicitly_wait(time)
-
+    # Driver를 종료시킴
     def exit(self):
         self.driver.close()
