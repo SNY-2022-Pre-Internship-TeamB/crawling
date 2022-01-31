a9cf71614d8ff3f76d87cb958117960aba4ee2f2
Jo Byeong Wook <whquddnr97@gmail.com>
2022-01-31 17:32:57 +0900

    changed logic > inject javascript

--- crawling.py
+++ crawling.py
@@ -1,14 +1,14 @@
 from driver import Driver
-import time
+from bs4 import BeautifulSoup
+import math
 
-youthCenter = 'https://www.youthcenter.go.kr'
-policy_housing_finance = ['//*[@id="pc_gnb"]/ul/li[1]/a/span',
-                          '//*[@id="srchFrm"]/div[2]/div[1]/div[2]/div[2]/ul/li[3]/span/button',
-                          '//*[@id="srchFrm"]/div[2]/div[1]/div[5]/div[2]/span/label',
-                          '//*[@id="srchFrm"]/div[2]/div[2]/a[2]']
-
-if __name__ == "__main__":
-    driver = Driver()
+def get_policy_num(driver):
+    youthCenter = 'https://www.youthcenter.go.kr'
+    policy_housing_finance = ['//*[@id="pc_gnb"]/ul/li[1]/a/span',
+                              '//*[@id="srchFrm"]/div[2]/div[1]/div[2]/div[2]/ul/li[3]/span/button',
+                              '//*[@id="srchFrm"]/div[2]/div[1]/div[5]/div[2]/span/label',
+                              '//*[@id="srchFrm"]/div[2]/div[2]/a[2]']
+    policy_numbers = []
 
     driver.get_url(youthCenter)
     print("사이트 접속 성공")
@@ -17,9 +17,42 @@ if __name__ == "__main__":
         driver.find_xpath_element(xpath).click()
     print("정책 선택 완료")
 
-    policy_num = driver.find_xpath_element('//*[@id="srchFrm"]/div[4]/div[1]/div[1]/strong/span').text
-    print('정책 갯수 : ', policy_num)
+    total_num = int(driver.find_xpath_element('//*[@id="srchFrm"]/div[4]/div[1]/div[1]/strong/span').text)
+    total_page_num = math.ceil(total_num / 12)
+    print('정책 갯수 : ', total_num, '페이지 수 : ', total_page_num)
 
-    link = driver.find_css_selector_element('#dtlLink_R2022010701021')
-    driver.execute_onclick(link)
-    time.sleep(10)
+    aTags = driver.find_css_selector_elements('#srchFrm > div.sch-result-wrap.compare-result-list > div.result-list-box > ul > li > a')
+    for aTag in aTags:
+        policy_number = str(aTag.get_attribute('id')[8:])
+        policy_numbers.append(policy_number)
+
+    print("{}개 정책의 정책번호 저장 완료".format(len(policy_numbers)))
+
+    return policy_numbers
+
+def inject_script(driver):
+    script = """
+    function f_Detail(key){
+        $("#srchAge").val($("#srchAge").val().replace(/[^0-9]/gi,""));
+        $("#bizId").val(key);
+        var srchFrm = document.srchFrm;
+        srchFrm.action = "/youngPlcyUnif/youngPlcyUnifDtl.do";
+        srchFrm.submit();}
+    """
+
+    driver.execute_script(script)
+    driver.execute_script("f_Detail('R2021021800114')")
+
+def parsing_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
+
+if __name__ == "__main__":
+    print("크롤링 시작")
+    driver = Driver()
+
+    policy_numbers = get_policy_num(driver)
+    driver.exit()
+    #parsing_html(result)
+
+
--- driver.py
+++ driver.py
@@ -22,6 +22,9 @@ class Driver:
     def get_url(self, url):
         self.driver.get(url)
 
+    def get_html(self):
+        return self.driver.page_source
+
     def find_xpath_element(self, xpath):
         return WebDriverWait(self.driver, 5, poll_frequency = 1).until(
             EC.presence_of_element_located((By.XPATH, xpath)),
@@ -34,8 +37,20 @@ class Driver:
             message = 'CSS_SELECTOR element not found'
         )
 
+    def find_css_selector_elements(self, css_selector):
+        return WebDriverWait(self.driver, 5, poll_frequency = 1).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, css_selector)),
+            message = 'CSS_SELECTOR elements not found'
+        )
+
+    def execute_script(self, script):
+        self.driver.execute_script(script)
+
     def execute_onclick(self, link):
         self.driver.execute_script("arguments[0].click();", link)
 
     def wait(self, time):
         self.driver.implicitly_wait(time)
+
+    def exit(self):
+        self.driver.close()
